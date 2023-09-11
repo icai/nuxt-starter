64905eceff8803e5846db9f6551d3e0201060885
Terry Cai <gidcai@gmail.com>
2023-09-11 21:57:18 +0800

    feat: remove useless files

--- client/lang/translate-client.js
+++
@@ -1,7 +0,0 @@
-import locizeActions from '@w3cub/locize-actions'
-import options from './config'
-locizeActions.init(options.init)
-
-export default (locale, path) => {
-  return locizeActions.actions('missing', options.namespace, path, path)
-}
