6c210493240f91f7d2127946b5c5d1add24def3c
Terry Cai <gidcai@gmail.com>
2023-09-12 18:24:25 +0800

    feat: remove useless

--- client/components/aui/_util/format.js
+++
@@ -1,8 +0,0 @@
-export const formatNumber = (value, isNumber, isDigit) => {
-  if (isNumber) {
-    value = value.replace(/[^\d]/g, '')
-  } else if (isDigit) {
-    value = value.replace(/[^\d.]/g, '')
-  }
-  return value
-}
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,6 @@ export default defineNuxtConfig({
   serverDir: "server",
   components: {
     dirs: [
-      '~/components/aui',
       '~/components/global'
     ]
   },
